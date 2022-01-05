d84f2f324cf4024055f84926b23945c028db33f1
AnonymousX <93084047+AnonymousX0-0@users.noreply.github.com>
2022-01-05 10:48:33 +0700

    Add files via upload

---
+++ tcp_ddos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import socket
+
+print("|----DDOS-PING----|")
+print("|----Anonymous----|")
+
+HOST = str(input(" IP:"))
+PORT = int(input(" PORT:"))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((str(HOST), int(PORT)))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
